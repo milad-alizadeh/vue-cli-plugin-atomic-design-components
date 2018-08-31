83e45e6a722d1254fccf5fdf588a293dca7f51fc
Milad Alizadeh <milaadd@gmail.com>
2018-08-31 11:20:48 +0100

    create form select

---
+++ generator/template/src/components/molecules/VFormSelect/index.stories.js
@@ -0,0 +1,133 @@
+import { storiesOf } from '@storybook/vue'
+import VueInfoAddon from 'storybook-addon-vue-info'
+import components from 'components'
+
+const wrapper = {
+  components,
+  propsDescription: {
+    label: 'Label text for the field',
+    required: 'Whether the field is required',
+    error: 'Whether select value is invalid',
+    success: 'Whether select value is valid',
+    value: 'Value of the select',
+    options: 'Available options for the select. A single option format is: { label: String, value: String, disabled: Boolean }',
+    disabled: 'Disabled attribute for the select',
+    errorMessage: 'The error message shown if the select validation is failed',
+    emptyValueLabel: 'Placeholder text for first item in the select'
+  }
+}
+
+const options = [
+  {
+    label: 'Option A',
+    value: 'a'
+  },
+  {
+    label: 'Option B',
+    value: 'b'
+  },
+  {
+    label: 'Option C',
+    value: 'c'
+  },
+  {
+    label: 'Option D',
+    value: 'd',
+    disabled: true
+  }
+]
+
+storiesOf('Molecules - VFormSelect', module)
+  .addDecorator(VueInfoAddon)
+  .add('default', () => ({
+    ...wrapper,
+    template: `
+      <div>
+        <VFormSelect
+          label="Choose your country"
+          emptyValueLabel="Please select a country"
+          v-model="frieldValue"
+          :options="options"
+        />
+
+        <br/><br/>
+
+        Field value is: {{ frieldValue }}
+      </div>
+    `,
+    data () {
+      return {
+        frieldValue: 'c',
+        options
+      }
+    }
+  }))
+  .add('invalid', () => ({
+    ...wrapper,
+    template: `
+      <VFormSelect
+        label="Choose your country"
+        v-model="frieldValue"
+        :options="options"
+        error
+        errorMessage="Please choose one of the options"
+      />
+    `,
+    data () {
+      return {
+        frieldValue: '',
+        options
+      }
+    }
+  }))
+  .add('valid', () => ({
+    ...wrapper,
+    template: `
+      <VFormSelect
+        label="Choose your country"
+        v-model="frieldValue"
+        :options="options"
+        success
+      />
+    `,
+    data () {
+      return {
+        frieldValue: 'a',
+        options
+      }
+    }
+  }))
+  .add('required', () => ({
+    ...wrapper,
+    template: `
+      <VFormSelect
+        label="Choose your country"
+        v-model="frieldValue"
+        :options="options"
+        required
+      />
+    `,
+    data () {
+      return {
+        frieldValue: '',
+        options
+      }
+    }
+  }))
+  .add('disabled', () => ({
+    ...wrapper,
+    template: `
+      <VFormSelect
+        label="Choose your country"
+        disabled
+        v-model="frieldValue"
+        :options="options"
+      />
+    `,
+    data () {
+      return {
+        frieldValue: '',
+        options
+      }
+    }
+  }))
---
+++ generator/template/src/components/molecules/VFormSelect/index.vue
@@ -0,0 +1,126 @@
+<template>
+  <div
+    :class="[
+      'v-m-form-select',
+      `${disabled ? 'v-m-form-select--disabled' : ''}`,
+      `${error ? 'v-m-form-select--error' : ''}`,
+      `${success ? 'v-m-form-select--success' : ''}`
+    ]"
+  >
+    <VLabel
+      v-if="label"
+      :forAttribute="`#${id}`"
+      :error="error"
+    >
+      <template v-if="required">* </template>{{ label }}
+    </VLabel>
+
+    <VSelect
+      :value="value"
+      :disabled="disabled"
+      :required="required"
+      :id="id"
+      :emptyValueLabel="emptyValueLabel"
+      :options="options"
+      @change="value => $emit('input', value)"
+    />
+
+    <VText v-if="error && errorMessage" size="x-small">{{ errorMessage }}</VText>
+  </div>
+</template>
+
+<script>
+import { uid } from '@/helpers'
+
+export default {
+  props: {
+    label: String,
+    options: {
+      type: Array,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true,
+      default: ''
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    emptyValueLabel: String,
+    errorMessage: String,
+    error: {
+      type: Boolean,
+      default: false
+    },
+    success: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    id () {
+      return uid()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.v-m-form-select {
+  .v-a-label {
+    margin-bottom: 1rem;
+  }
+
+  .v-a-select {
+    margin-bottom: 1rem;
+  }
+
+  &--error {
+    .v-a-text,
+    .v-a-label,
+    .v-a-select {
+      color: $error
+    }
+
+    .v-a-select__input {
+      border-color: $error
+    }
+
+    .v-a-select__arrow {
+      fill: $error
+    }
+  }
+
+  &--success {
+    .v-a-text,
+    .v-a-label,
+    .v-a-select {
+      color: $success
+    }
+
+    .v-a-select__input {
+      border-color: $success
+    }
+
+    .v-a-select__arrow {
+      fill: $success
+    }
+  }
+
+  &--disabled {
+    .v-a-select {
+      background: $grey-lighter;
+    }
+
+    .v-a-label {
+      color: $grey;
+    }
+  }
+}
+</style>
