50a38aa37c1c72e9b61720d5c0480bd018ac6c0a
Milad Alizadeh <milaadd@gmail.com>
2018-10-02 17:21:55 +0100

    update generator

--- generator/index.js
+++ generator/index.js
@@ -1,7 +1,7 @@
 module.exports = (api, options, rootOptions) => {
   api.extendPackage({
     dependencies: {
-      vuelidate: "^0.7.4",
+      "vuelidate": "^0.7.4",
       "vue-tel-input": "^2.0.5",
       "credit-card-type": "^7.1.0"
     }
@@ -9,15 +9,19 @@ module.exports = (api, options, rootOptions) => {
 
   api.render('./template')
 
-  api.injectImports('src/main.js', `import Vuelidate from 'vuelidate'`)
-
   api.postProcessFiles(files => {
+    let webapp = files['src/web-app.js']
     let main = files['src/main.js']
+
+    if (webapp) {
+      main = webapp
+    }
+
     if (main) {
       const lines = main.split(/\r?\n/g).reverse()
       const lastImportIndex = lines.findIndex(line => line.match(/^import/))
-      lines[lastImportIndex] += `\n Vue.use(Vuelidate)`
-      main = lines.reverse().join('\n')
+      lines[lastImportIndex] += `\nimport Vuelidate from 'vuelidate'\nVue.use(Vuelidate)`
+      files['src/web-app.js'] = lines.reverse().join('\n')
     }
   })
 }
